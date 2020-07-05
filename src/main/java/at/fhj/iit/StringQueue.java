ebaf2fc43f6f54b0a746a2fe8c07925da7e09041
weTobias <tobias.weigl99@gmail.com>
2020-07-05 03:41:10 +0200

    added descriptions for every method

--- src/main/java/at/fhj/iit/StringQueue.java
+++ src/main/java/at/fhj/iit/StringQueue.java
@@ -10,13 +10,32 @@ import java.util.NoSuchElementException;
 
 public class StringQueue implements IQueue <String>{
 
+  /**
+   * List of all elements within the queue
+   */
   private List<String> elements = new ArrayList<String>();
+
+  /**
+   * Determines the maximum size of the queue
+   */
   private int maxSize;
 
+  /**
+   * Constructor of the queue which sets its maximum size
+   *
+   * @param maxSize to set the maximum size of the queue
+   */
   public StringQueue(int maxSize) {
     this.maxSize = maxSize;
   }
 
+  /**
+   * Adds the element obj to the queue.
+   * If the addition is successful, the method returns true else false.
+   *
+   * @param obj is a new element which wants to get added to the queue
+   * @return whether or not the element was added successfully
+   */
   @Override
   public boolean offer(String obj) {
     if (elements.size() != maxSize){
@@ -26,6 +45,12 @@ public class StringQueue implements IQueue <String>{
     return false;
   }
 
+  /**
+   * Returns the head (first) element and also deletes it. That is, we cannot get it again.
+   * If no element exists (when queue is empty), the method returns null.
+   *
+   * @return the first element of the queue
+   */
   @Override
   public String poll() {
     if (elements.size() != 0){
@@ -36,6 +61,14 @@ public class StringQueue implements IQueue <String>{
     return null;
   }
 
+  /**
+   * It also returns and deletes the head element like poll(), but with a small difference.
+   * This method throws NoSuchElementException if the queue is empty.
+   *
+   * @throws NoSuchElementException when the queue is empty
+   *
+   * @return the first element of the queue
+   */
   @Override
   public String remove() {
     String element = peek();
@@ -46,6 +79,12 @@ public class StringQueue implements IQueue <String>{
     return element;
   }
 
+  /**
+   * Returns the head element but it does not delete it. That is, we can get it again.
+   * Returns null when the queue is empty.
+   *
+   * @return the first element of the queue
+   */
   @Override
   public String peek() {
     if (elements.size() > 0){
@@ -54,6 +93,14 @@ public class StringQueue implements IQueue <String>{
     return null;
   }
 
+  /**
+   * It works similar to peek() but with a small difference (returns but does not delete the element).
+   * It throws NoSuchElementException when the queue is empty.
+   *
+   * @throws NoSuchElementException when the queue is empty
+   *
+   * @return the first element of the queue
+   */
   @Override
   public String element() {
     if (peek() == null){
